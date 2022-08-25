dc15b28de79079f09854faeb58e495f4588037b8
melody <CelestialMelodyGo@gmail.com>
2022-08-25 23:36:41 +0800

    update

--- pkg/validate/valid.go
+++ pkg/validate/valid.go
@@ -4,10 +4,10 @@ import "github.com/go-playground/validator/v10"
 
 var valid = validator.New()
 
-func Var(field interface{}, tag string) error {
-	return valid.Var(field, tag)
-}
-
 func Struct(s interface{}) error {
 	return valid.Struct(s)
+}
+
+func Var(field interface{}, tag string) error {
+	return valid.Var(field, tag)
 }
